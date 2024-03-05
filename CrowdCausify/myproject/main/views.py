d3668dc0a3fb5e2090fb55ae07d13868764725a5
TwoUfo <94078255+TwoUfo@users.noreply.github.com>
2024-03-05 19:55:53 +0200

    edited views and urls

--- CrowdCausify/myproject/main/views.py
+++ CrowdCausify/myproject/main/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+# def home(request):
+#     return render(request, 'home.html')
+
+def home(request):
+    return HttpResponse("Log in via google")
--- CrowdCausify/myproject/myproject/urls.py
+++ CrowdCausify/myproject/myproject/urls.py
@@ -21,5 +21,6 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('login/google/', RedirectView.as_view(url='/accounts/google/login/')),  # Додаємо посилання для входу через Google
+    path('login/google/', RedirectView.as_view(url='/accounts/google/login/')),
+    path('', include('main.urls')),
 ]
