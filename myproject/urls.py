960105a5858fbd8ef743fc0289f9f9a7802cfed6
TwoUfo <94078255+TwoUfo@users.noreply.github.com>
2024-04-10 15:41:55 +0300

    update requirements

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,6 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('login/google/', RedirectView.as_view(url='/accounts/google/login/')),
     path('', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
